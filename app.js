ad22ff2e37145d770d55f50d8ecfa0b7c6c7e335
Fimaye Dadi <dadifirmaye@gmail.com>
2023-08-06 09:11:52 +0300

    all website made responsive

---
+++ app.js
@@ -0,0 +1,7 @@
+var bt = document.querySelector(".btn");
+const link = document.querySelector(".links");
+bt.addEventListener("click", function () {
+  link.classList.toggle("link-show");
+  console.log("link");
+});
+console.log("link");
